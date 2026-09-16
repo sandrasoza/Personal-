f8b3cb1db7ed2d65b9d9d05f4c7065aa743fde51
sandra soza <ssozazambrano@luc.edu>
2026-09-15 23:32:53 -0500

    Implementation of Main

---
+++ terminalEmulator.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <signal.h>
+
+/* This is a simple terminal emulator implementation in C */
+// argc = count of arguments
+// argv = array of argument strings
+//Reads input from the terminal//
+int main(int argc, char *argv[]){
+
+}
